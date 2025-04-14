03b5d87fe6c69af7403458b8b007d96c25afcbdc
wuxiaodong <huey.wu@hkai.io>
2025-04-14 10:10:23 +0800

    push test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import MyButton from "@/components/btn";
 import MarkManage from "@/components/MarkManage";
 
 export default function Home() {
+  console.log("push==test")
   return (
     <>
       <section className="m-20">
